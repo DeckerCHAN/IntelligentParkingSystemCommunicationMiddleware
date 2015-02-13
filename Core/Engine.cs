7f276cacc58e674ca1068637f1ea230206aeb6fc
DeckerCHAN <DeckerCHAN@gmail.com>
2015-02-13 14:17:46 +0800

    Initial commit.

---
+++ Core/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GUI;
+
+namespace Core
+{
+    class Engine:ApplicationContext
+    {
+        private static Engine _instance;
+
+        public static Engine GetEngine()
+        {
+            return _instance ?? (_instance = new Engine());
+        }
+
+        #region fields
+
+        #endregion
+        
+        private Engine()
+        {
+            this.MainForm=new MainForm();
+        }
+    }
+}
