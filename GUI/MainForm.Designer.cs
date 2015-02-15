7ac024ba7fdaa7171e815f5a01cd37973b653ecb
DeckerCHAN <DeckerCHAN@gmail.com>
2015-02-15 11:46:46 +0800

    Added project name to assebly and refactored something in ConsoleForm

--- GUI/MainForm.Designer.cs
+++ GUI/MainForm.Designer.cs
@@ -1,11 +1,14 @@
-﻿namespace IPSCM.GUI
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace IPSCM.GUI
 {
     partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,11 +31,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConsoleTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ConsoleTextBox
+            // 
+            this.ConsoleTextBox.BackColor = System.Drawing.Color.Black;
+            this.ConsoleTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ConsoleTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConsoleTextBox.ForeColor = System.Drawing.Color.Lime;
+            this.ConsoleTextBox.Location = new System.Drawing.Point(0, 0);
+            this.ConsoleTextBox.Name = "ConsoleTextBox";
+            this.ConsoleTextBox.Size = new System.Drawing.Size(636, 388);
+            this.ConsoleTextBox.TabIndex = 0;
+            this.ConsoleTextBox.Text = "";
+            this.ConsoleTextBox.TextChanged += new System.EventHandler(this.ConsoleTextBox_TextChanged);
+            this.ConsoleTextBox.ReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(636, 388);
+            this.Controls.Add(this.ConsoleTextBox);
+            this.Name = "MainForm";
+            this.Text = "Console";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private RichTextBox ConsoleTextBox;
+
+
     }
 }
--- GUI/MainForm.cs
+++ GUI/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace IPSCM.GUI
 {
@@ -8,5 +10,32 @@ namespace IPSCM.GUI
         {
             InitializeComponent();
         }
+
+        private void ConsoleTextBox_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public void Out(String text)
+        {
+            this.ConsoleTextBox.AppendText(text + Environment.NewLine);
+            this.ConsoleTextBox.ScrollToCaret();
+        }
+        public void Out(String text, Color color)
+        {
+            this.AppendText(text + Environment.NewLine, color);
+            this.ConsoleTextBox.ScrollToCaret();
+        }
+
+        private void AppendText(string text, Color color)
+        {
+            this.ConsoleTextBox.SelectionStart = this.ConsoleTextBox.TextLength;
+            this.ConsoleTextBox.SelectionLength = 0;
+
+            this.ConsoleTextBox.SelectionColor = color;
+            this.ConsoleTextBox.AppendText(text);
+            this.ConsoleTextBox.SelectionColor = this.ConsoleTextBox.ForeColor;
+        }
+
     }
 }
