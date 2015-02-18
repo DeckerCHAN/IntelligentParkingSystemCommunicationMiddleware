3fe7e3a0dbc3a90d872cc758bf8249c76cf2563b
DeckerCHAN <DeckerCHAN@gmail.com>
2015-02-18 22:38:22 +0800

    Added porject Utils

---
+++ Utils/HashUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IPSCM.Utils
+{
+    public static class HashUtils
+    {
+        public static String CalculateMD5Hash(String input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Utils/NetWorkUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace IPSCM.Utils
+{
+    public static class NetWorkUtils
+    {
+        public static Boolean IsAvailablePortNumber(UInt32 port)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            return tcpConnInfoArray.All(tcpi => tcpi.LocalEndPoint.Port != port);
+        }
+    }
+}
