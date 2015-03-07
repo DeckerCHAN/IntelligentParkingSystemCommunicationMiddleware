60294d77af3d89357ea20361385e18968a5ec486
DeckerCHAN <DeckerCHAN@gmail.com>
2015-03-07 10:53:32 +0800

    Added Persistence

---
+++ Core/Storage/DBScripts/Init.sql
@@ -0,0 +1,38 @@
+﻿IF db_id('IPSCM') IS NOT NULL
+BEGIN
+    SELECT 'database does exist'
+END
+ELSE
+BEGIN
+    SELECT 'database does not exist'
+	CREATE DATABASE IPSCM;	
+END
+GO
+USE [IPSCM] 
+GO
+--Create Users if users not exists
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [Users]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+	[PlateNumber] NVARCHAR(30) NOT NULL, 
+	[Money] MONEY NOT NULL, 
+	[PhoneNumber] CHAR(20) NOT NULL
+)	
+END
+GO
+--Create Ticket if ticket not exists
+IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Tickets]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [Tickets]
+(
+	[TicketId] INT NOT NULL PRIMARY KEY, 
+	[Type] VARCHAR(30) NOT NULL, 
+	[Money] MONEY NOT NULL, 
+	[Value] INT NOT NULL,
+	[UserId] INT NOT NULL, 
+	[StorageName] NVARCHAR(50) NOT NULL
+)
+END
+GO
---
+++ Persistence/Initialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPSCM.Persistence
+{
+    public static class Initialization
+    {
+        public static Boolean IsDBExists()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
