e45a9743e152ae6b99e0fa4f39ad0876ec95fc44
DeckerCHAN <DeckerCHAN@gmail.com>
2015-03-08 00:24:13 +0800

    Added sql splitor as util.

---
+++ Utils/SQLUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPSCM.Utils
+{
+    public static class SqlUtils
+    {
+        private static String[] SqlFileSplitor = new []{"GO\r\n", "GO ", "GO\t"};
+        public static String[] SplitSqlFile(String fileContent)
+        {
+            return fileContent.Split(SqlFileSplitor , StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
