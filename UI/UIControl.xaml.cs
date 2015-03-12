dd17b8b2e111a6a0528fa174b5e1ca90883263d3
DeckerCHAN <DeckerCHAN@gmail.com>
2015-03-12 09:21:02 +0800

    Added new project as new gui

---
+++ UI/UIControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for UIControl.xaml
+    /// </summary>
+    public partial class UIControl : Application
+    {
+        public UIControl()
+        {
+            MainWindow mainWindow = new MainWindow();
+            throw new Exception();
+        }
+    }
+}
